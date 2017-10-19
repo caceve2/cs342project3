5821ecca312dcef5e59116727b93e5c03296e8bb
caceve2 <32916092+caceve2@users.noreply.github.com>
2017-10-19 14:30:04 -0500

    new stuff

--- src/cs342project3/board.java
+++ src/cs342project3/board.java
@@ -3,6 +3,9 @@ package cs342project3;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.plaf.ColorUIResource;
+import javax.swing.plaf.FontUIResource;
+
 import java.awt.event.*;
 import java.util.*;
 
@@ -17,6 +20,7 @@ public class board extends JFrame
 	private final JPanel[][] miniPanels;
 	private JButton PANEL[];
 	private String panelArray[] = {"1","2","3","4","5","6","7","8","9","X"};
+	private String SELECTEDNUMBER;
 
 	public board(String puzzle[][])
 	{
@@ -26,25 +30,29 @@ public class board extends JFrame
 		Font f = new Font ("Serif" , Font.BOLD , 12);
 
 		Cells = new JButton[GRIDSIZE][GRIDSIZE];
-		
+		PANELHandler PH = new PANELHandler();
+		GRIDHandler GH = new GRIDHandler();
 		
 
 		GRID = new JPanel(new GridLayout(GRIDSIZE,GRIDSIZE));
 		for(int i = 0; i < GRIDSIZE; i++)
 		for ( int j = 0; j < GRIDSIZE; j++ )
 		{
+			UIManager.put("Cells[i][j].disabledForeground", Color.RED);
+			
 			Cells[i][j] = new JButton(puzzle[i][j]);
-
+			Cells[i][j].addActionListener(GH);
 			
 			GRID.add( Cells[i][j]);
+			
 			Cells[i][j].setBackground(Color.WHITE);
+			
 			
 			if (Cells[i][j].getText() != "")
 			{
 				
-				Cells[i][j].setBackground(Color.GREEN);
+				Cells[i][j].removeActionListener(GH);
 				
-				Cells[i][j].setFont(f);
 				
 			}
 			
@@ -80,11 +88,13 @@ public class board extends JFrame
 		
 		nestedPanel = new JPanel(new GridLayout(10,1),false);
 		PANEL = new JButton[10];
-		Color color  = new Color(255,0,0);
+		
 		for ( int j = 0; j <= 9; j++ )
 		{
 			PANEL[j] = new JButton(panelArray[j]);
-			PANEL[j].setBackground(color);
+			PANEL[j].addActionListener(PH);
+			PANEL[j].setForeground(Color.WHITE);
+			PANEL[j].setBackground(Color.BLACK);
 			
 			nestedPanel.add(PANEL[j] );
 		}
@@ -93,6 +103,55 @@ public class board extends JFrame
 
 		container.add(GRID, BorderLayout.CENTER);
 		container.add(nestedPanel, BorderLayout.EAST);
+	}
+	
+	private class PANELHandler implements ActionListener
+	{
+		public void actionPerformed( ActionEvent event )
+		{
+			JButton pushed = (JButton) event.getSource();
+			
+			for ( int count = 0; count < 10; count++ )
+			{
+				if(PANEL[count] == pushed)
+				{
+
+					SELECTEDNUMBER = pushed.getText();
+				}
+
+			}
+			
+		}
+
+	}
+	
+	private class GRIDHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
+		{
+			JButton pushed = (JButton) event.getSource();
+			
+		for(int i = 0 ; i < GRIDSIZE; i++)
+			for ( int j = 0; j < GRIDSIZE; j++ )
+			{
+				if(Cells[i][j] == pushed)
+				{
+		
+					
+						if(SELECTEDNUMBER == "X")
+							Cells[i][j].setText("");
+						else
+							Cells[i][j].setText(SELECTEDNUMBER);
+					
+					
+					
+					
+					
+				}
+
+			}
+			
+		}
 	}
 
 
