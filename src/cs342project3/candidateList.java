264097cf62a83413313ef8dba99ed4e5ac5b0d50
caceve2 <32916092+caceve2@users.noreply.github.com>
2017-10-21 17:20:02 -0500

    new stuff(not done)

---
+++ src/cs342project3/candidateList.java
@@ -0,0 +1,24 @@
+package cs342project3;
+
+import javax.swing.JButton;
+
+public class candidateList extends board {
+	
+	private String[] candidateArray = new String[9];
+	
+	public candidateList(String[][] puzzle, int posX , int posY) 
+	{
+		super(puzzle);
+		
+		for(int i = 0 ; i < GRIDSIZE; i++)
+		{
+			
+		}
+		
+		
+	}
+
+
+	
+	
+}
